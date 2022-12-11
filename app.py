b67942e5b11713c170c189c40a0fb29e6558c374
rebuzik <62416511+rebuzik@users.noreply.github.com>
2022-12-11 15:33:44 +0800

    приложение с 2 моделями

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, request, render_template
 import pickle
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 app = flask.Flask(__name__, template_folder='templates')
 
-loaded_model = pickle.load(open("Random_Forest.pkl", 'rb'))
+loaded_model = pickle.load(open("Random_Forest_for_Depth.pkl", 'rb'))
+loaded_model_for_Width = pickle.load(open("Random_Forest_for_Width.pkl", 'rb'))
 scaler_X = pickle.load(open("scaler_X2.pkl", 'rb'))
 
 @app.route('/', methods = ['POST', 'GET'])
@@ -31,10 +33,9 @@ def main():
         print(X_list_for_predict)
 
         X_list_for_predict_scaled = scaler_X.transform([X_list_for_predict])
-        y_pred = loaded_model.predict(X_list_for_predict_scaled)
-
-
-        return render_template('main.html', result = y_pred, result2 = y_pred)
+        y_pred_Depth = loaded_model.predict(X_list_for_predict_scaled)
+        y_pred_Width = loaded_model_for_Width.predict(X_list_for_predict_scaled)
+        return render_template('main.html', result = [round(float(np.exp(y_pred_Depth)), 4) , round(float(np.exp(y_pred_Width)), 4)])
 
 if __name__ == '__main__':
     app.run(debug = True)
